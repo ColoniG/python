0e4878c8dc2e9dc0d36a529c3dbbabab01150ec1
ColoniG <123653994+ColoniG@users.noreply.github.com>
2025-02-11 16:42:00 -0300

    Update Aula08.py

---
+++ mundo01/Aula08.py
@@ -0,0 +1,7 @@
+import math
+#print(f'O valor a pagar é R$ {60 * 8.0 + (.15 * 8):.2f}')
+
+num = int(input('Digite um número: '))
+raiz =  math.sqrt(num)
+
+print(f'A raiz quadrada de {num} é {raiz:.2f}')
